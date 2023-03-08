1ae84c868d6c26b94f90b696e937d3a1491ff51b
uxknzm <muazzelinkic@gmail.com>
2023-03-08 17:14:50 +0300

    test commit

--- index.js
+++ index.js
@@ -33,14 +33,10 @@ app.use(cors({
 async function getUserDataFromRequest(req) {
   return new Promise((resolve, reject) => {
     const token = req.cookies?.token;
-    if (token) {
       jwt.verify(token, jwtSecret, {}, (err, userData) => {
         if (err) throw err;
         resolve(userData);
       });
-    } else {
-      reject('no token');
-    }
   });
 
 }
